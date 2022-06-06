26e9e8fa0602688839a7cc6ba518f82852091161
Moises Eskinazi <moiesk@gmail.com>
2022-06-06 09:51:22 -0400

    Initial commit

---
+++ packages/polls/list/__main__.py
@@ -0,0 +1,34 @@
+import os
+from urllib.parse import urlparse
+import psycopg2
+
+def main(args):
+  
+  dbUrl = os.environ['DATABASE_URL']
+  p = urlparse(dbUrl)
+
+  pg_connection_dict = {
+    'dbname': p.path.strip(),
+    'user': p.username,
+    'password': p.password,
+    'port': p.port,
+    'host': p.hostname,
+    'sslmode': 'require'
+  }
+  query = "SELECT COUNT(1) as count FROM polls_question"
+  
+  conn = psycopg2.connect(**pg_connection_dict)
+  cur = conn.cursor()
+  
+  cur.execute(query)
+  result = cur.fetchone()
+  
+  cur.close()
+  conn.close()
+
+  return {
+    'body': {
+      'response_type': 'in_channel',
+      'question_count': result['count']
+    }
+  }
